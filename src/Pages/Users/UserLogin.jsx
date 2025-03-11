c13cccf61a45a999674d1bf2a0b350b42d40e561
Atlas0007 <Eniolaorimalade@gmail.com>
2025-03-11 10:20:18 +0100

    Additions

--- src/Pages/Users/UserLogin.jsx
+++ src/Pages/Users/UserLogin.jsx
@@ -6,8 +6,24 @@ import react from 'react'
 
   return (
     <>
-      <h1>User Login</h1>
-      <p>Eniola Webpage</p>
+     
+<form>
+  <label htmlFor="email" style={{fontSize: "20px" }}> Email or Phone : </label>
+  <input type="text"  id= "email" placeholder='Enter your email' style={{borderRadius: "5px", fontSize : "20px"}}/>
+
+<br />
+<br />
+
+  <label htmlFor="password">Password: </label>
+  <input type="text" id ="password" placeholder='password?' />
+
+<br />
+<br />
+
+<label htmlFor="submit">Submit : </label>
+<button type='submit' style={{backgroundColor : "white", padding:"5px"}}> Log in</button>
+</form>
+      
     </>
   )
 }
